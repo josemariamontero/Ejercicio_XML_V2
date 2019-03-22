4b79b08016401c33f04f134e05f119b1330b8a4e
Jose Maria Montero <josema9817@gmail.com>
2019-03-22 14:46:25 +0100

    Finalizado

--- ejercicio5.py
+++ ejercicio5.py
@@ -15,14 +15,8 @@ radares,latitud,longitud,provincia_radar = coordenadas(carretera,doc)
 
 print ("Numero de radares:",radares)
 
-#for a,i,x in zip(provincia_radar,latitud,longitud):
-	#print ("http://www.openstreetmap.org/search?query=%s#map=15/45.3325/-100.1207/%s/%s" % (a,i,x))
-
 for i,x in zip(latitud,longitud):
-	#print("https://www.openstreetmap.org/search?query=%s-1.50566#map=8/%s/%s" % (i,i,x))
-	print ("http://www.openstreetmap.org/search?query=%s#map=15/45.3325/-100.1207/%s/%s" % (i,i,x))
+	print("https://www.openstreetmap.org/search?query=%s,%s#map=19/%s/%s" % (i,x,i,x))
 
 #//CARRETERA[DENOMINACION="N-234"]/RADAR/PUNTO_INICIAL/LATITUD/text()
 #//CARRETERA[DENOMINACION="N-234"]/RADAR/PUNTO_INICIAL/LONGITUD/text()
-#print ("http://www.openstreetmap.org/search?query=akaska#map=15/45.3325/-100.1207/%s/%s" % (latitud,longitud))
-#https://www.openstreetmap.org/search?query=41.84701532%2C-3.11377559#map=8/41.847/-3.115
